f5d3ce08a5d8f40ba6ee2e07e0af15e426c298f9
Mattias Johansson <Mattias.Johansson@volvo.com>
2022-12-01 14:31:02 +0100

    refactor

--- Day1/Day1.py
+++ Day1/Day1.py
@@ -3,11 +3,7 @@ import pandas as pd
 with open("input.txt") as f:
     lines = [i.replace("\n", "") for i in f.readlines()]
 
-elfnbr = []
-elfcalories = []
-largest = 0
-bank = 0
-
+elfnbr, elfcalories, largest, bank = [], [], 0, 0
 for idx, i in enumerate(lines):
     if len(i) > 0:
         bank += int(i)
@@ -15,8 +11,5 @@ for idx, i in enumerate(lines):
         elfnbr.append(idx)
         elfcalories.append(bank)
         bank = 0
-    if bank > largest:
-        largest = bank
-
 df = pd.DataFrame({"ElfNumber": elfnbr, "ElfCalories": elfcalories})
-print(df.sort_values("ElfCalories").tail(3)["ElfCalories"].sum())
+print(df.sort_values("ElfCalories").tail(3)["ElfCalories"])
