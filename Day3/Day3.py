fcfe3a976aa5ffaf0ada2edf586c721d9e6f0064
Mattias Johansson <Mattias.Johansson@volvo.com>
2022-12-03 11:40:29 +0100

    Day 3 solution compact solution

--- Day3/Day3.py
+++ Day3/Day3.py
@@ -5,10 +5,4 @@ with open(input) as f:
     lines = [i.replace("\n", "") for i in f.readlines()]
 
 alphabet_map = {**{chr(96 + i): ord(chr(96 + i)) - 96 for i in range(1, 27)}, **{chr(96 - 26 + i).upper(): ord(chr(96 - 26 + i)) - 96 + 26 for i in range(27, 53)}}
-total_sum = []
-for rucksack in lines:
-    length = len(rucksack)
-    compartment1, compartment2 = rucksack[0 : int(length / 2)], rucksack[int(length / 2) : :]
-    total_sum.append((alphabet_map[set(compartment1).intersection(set(compartment2)).pop()]))
-
-print(sum(total_sum))
+print(sum([(alphabet_map[set(rucksack[0 : int(len(rucksack) / 2)]).intersection(set(rucksack[int(len(rucksack) / 2) : :])).pop()]) for rucksack in lines]))
