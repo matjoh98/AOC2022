edc9db889e89ce7d3b2f7dbb52db8132e35516e6
Mattias Johansson <Mattias.Johansson@volvo.com>
2022-12-05 08:42:31 +0100

    Day 5 solution

---
+++ Day5/Day4.py
@@ -0,0 +1,31 @@
+def prepare_stack(input_stack):
+    with open(input_stack) as f:
+        lines_stack = [i.replace("\n", "").split() for i in f.readlines()]
+    dic = {}
+    for i in lines_stack:
+        dic = {**dic, **{i[0]: i[1::]}}
+    return dic
+
+
+# input_stack = "example_stack.txt"
+input_stack = "stack.txt"
+
+# input = "example.txt"
+input = "input.txt"
+
+with open(input) as f:
+    lines = [i.replace("\n", "") for i in f.readlines()]
+
+stack_dict = prepare_stack(input_stack)
+for i in lines:
+    sentence = i.split()
+    times = int(sentence[1])
+    source = sentence[3]
+    destination = sentence[5]
+    for i in range(times):
+        stack_dict[destination].append(stack_dict[source].pop())
+
+new = []
+for i in stack_dict:
+    new.append(stack_dict[i].pop().replace("[", "").replace("]", ""))
+print(str(new).replace("[", "").replace("]", "").replace("'", "").replace(",", "").replace(" ", ""))
